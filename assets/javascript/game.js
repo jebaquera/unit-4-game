b225cf88cd38a22f32377042e1887af6819828b5
jebaquera <jebaquera@gmail.com>
2019-06-01 22:01:14 -0500

    updates to all files.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,24 @@
 $(document).ready(function(){
 
-// Generates random number to guess
+// Generate a random number for player to guess
 var Random = Math.floor(Math.random() * 102 + 19);
     
-// Display random number
+// Display random number to player
 $("#scoreToMatch").text(Random);
 
-//Generate random number for each crystal
-var numRed = Math.floor(Math.random()*12+1)
-var numYellow = Math.floor(Math.random()*12+1)
-var numGreen = Math.floor(Math.random()*12+1)
-var numBlue = Math.floor(Math.random()*12+1)
+// Generate random number for each crystal
+var numRed = Math.floor(Math.random() * 12 + 1)
+var numYellow = Math.floor(Math.random() * 12 + 1)
+var numGreen = Math.floor(Math.random() * 12 + 1)
+var numBlue = Math.floor(Math.random() * 12 + 1)
 
-// Variables to keep track of wins, losses and total
+// Variables to keep track of wins, losses and total player score
 var playerTotal = 0; 
 var wins = 0;
 var losses = 0;
 
-
 $("#wins").text(wins);
 $("#losses").text(losses); 
-
 
 // Reset game
 function reset(){
@@ -33,27 +31,29 @@ function reset(){
     numBlue = Math.floor(Math.random() * 12 + 1);
     playerTotal = 0;
     $("#userScore").text(playerTotal);
-    } 
+}
+
 // Display wins
 function congrats(){
-alert("Congrats! You did it!");
+alert("Congrats! You rock!");
 wins++; 
 $("#wins").text(wins);
 reset();
 }
 // Display losses
 function loser(){
-alert ("Sorry! You lose!");
+alert("Sorry! You lose!");
 losses++;
 $("#losses").text(losses);
 reset()
 }
+
 // Clicking crystals
 $(".red-crystal").on ("click", function(){
   playerTotal = playerTotal + numRed;
   console.log("New playerTotal = " + playerTotal);
   $("#userScore").text(playerTotal); 
-        //Win & lose conditions
+      //Win & lose conditions
       if (playerTotal === Random){
         congrats();
       }
@@ -64,7 +64,8 @@ $(".red-crystal").on ("click", function(){
 $(".yellow-crystal").on ("click", function(){
   playerTotal = playerTotal + numYellow;
   console.log("New playerTotal = " + playerTotal);
-  $("#userScore").text(playerTotal); 
+  $("#userScore").text(playerTotal);
+      //Win & lose conditions 
       if (playerTotal === Random){
         congrats();
       }
@@ -76,7 +77,8 @@ $(".green-crystal").on ("click", function(){
   playerTotal = playerTotal + numGreen;
   console.log("New playerTotal = " + playerTotal);
   $("#userScore").text(playerTotal);
-        if (playerTotal === Random){
+      //Win & lose conditions
+      if (playerTotal === Random){
         congrats();
       }
       else if (playerTotal > Random){
@@ -86,9 +88,9 @@ $(".green-crystal").on ("click", function(){
 $(".blue-crystal").on ("click", function(){
   playerTotal = playerTotal + numBlue;
   console.log("New playerTotal = " + playerTotal);
-  $("#userScore").text(playerTotal); 
-    
-        if (playerTotal === Random){
+  $("#userScore").text(playerTotal);
+      //Win & lose conditions 
+      if (playerTotal === Random){
         congrats();
       }
       else if (playerTotal > Random){
